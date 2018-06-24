e5e461343c506f8055291d1e8df37dfa6276d3e9
helei5 <helei5@xiaomi.com>
2018-06-24 12:03:46 +0800

    modify start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,26 @@
 #! /bin/sh
+cd `dirname $0`
+
+# 检查日志文件是否存在
+logpath='./logs'
+if [ ! -d $logpath ]; then
+  echo 'make log dir'
+  mkdir -p $logpath
+fi
+
+# 生成项目文件
+wwwpath='./www/test'
+if [ ! -d $wwwpath ]; then
+  echo 'make www dir'
+  mkdir -p $wwwpath
+fi
+
+# 生成php info文件
+testphp='./www/test/index.php'
+if [ ! -f $testphp ]; then
+  echo 'touch php file'
+  touch $testphp
+  echo '<?php\n phpinfo();' > $testphp
+fi
+
 docker-compose up -d
