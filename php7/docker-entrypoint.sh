1285105b0f53e781d08097c7f5359731097b526c
helei5 <helei5@xiaomi.com>
2018-06-26 10:19:56 +0800

    remove chown www dir

--- php7/docker-entrypoint.sh
+++ php7/docker-entrypoint.sh
@@ -9,7 +9,7 @@ if [ "$1" = 'php-fpm' -a "$(id -u)" = '0' ]; then
 	exec php-fpm
 
 	# 这部分由于项目多的时候，执行会非常慢，放在 php fmp启动后执行
-	chown -R work:work /home/work/www
+	#chown -R work:work /home/work/www
 fi
 
 exec "$@"
